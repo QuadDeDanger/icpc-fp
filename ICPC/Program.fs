33b93203979f7f29dfb4897656019800a5631baf
g16m3797 <g16m3797@campus.ru.ac.za.here>
2019-04-17 01:12:16 +0200

    error cases done

--- ICPC/Program.fs
+++ ICPC/Program.fs
@@ -12,18 +12,20 @@ let commaSprinkler (input:string) =
            |true -> None
            |_-> match input.[input.Length-1]='.' && input.[input.Length-1]<>' ' with
                 |false-> None
-                |true-> Some input
+                |true-> match input.Contains(',') || input.Contains("  ") || input.Contains(" ,") || input.Contains(" .") || input.Contains("...") || input.Contains(". .") with
+                        |true -> None
+                        |_-> Some input
 
 let rivers (input:string) =
- let space = input.Split(' ')
- match input.Length=0 with
- |true -> None
- |false->
- match input.Length<>0 || input.Contains('!') || input.Contains(',') || input.[input.Length-1]= ' ' || input.[0] = ' ' with
- |true -> None
- |_-> match  input.Contains(' ') with
-      |false-> None
-      |true -> Some input.Length
+ //let space = input.Split(' ')
+  match input.Length=0 with
+  |true -> None
+  |false->
+         match input.Length<>0 || input.Contains('!') || input.Contains(',') || input.[input.Length-1]= ' ' || input.[0] = ' ' with
+         |true -> None
+         |_-> match  input.Contains(' ') with
+              |false-> None
+              |true -> Some input.Length 
 
 [<EntryPoint>]
 let main argv =
